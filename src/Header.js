531004f99ab08da5de8e3255df4075aaf15ab06d
Brenda Long <brenda@studio6thave.com>
2022-01-27 14:48:39 -0600

    update css and added logos

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 
 export const Header = () => {
+
 
 	return (
 		<Container fluid="sm" id="header">
@@ -11,13 +12,24 @@ export const Header = () => {
 				</Col>
 			</Row>
 			<Row>
-				<Col xs={8}>
+				<Col sm={12} md={8}>
 					<blockquote><h1>Mabell's Zoo is a top-rated educational children's app.</h1>
 						<p>Come play with the animals of Mabell's Zoo; a beautiful experience for children that promotes creativity while counting to 10. Each view features an animal with original art, rich colors and textures, musical sounds, and a large play area.</p>
 					</blockquote>
 				</Col>
-				<Col xs={4}>
-					<img src={require(`./images/mz_icon_72.png`)} width="72" height="72" alt="Mabell's Zoo Icon" />
+				<Col sm={12} md={4}>
+					<Container className="storebuttons">
+						<div className="d-grid gap-2">
+						<div><img src={require(`./images/mz_icon_72.png`)} width="72" height="72" alt="Mabell's Zoo Icon" /> <br />Mabell's Zoo is Available Now</div>
+							<Button variant="primary" size="lg" onClick={() => window.location.href='https://apps.apple.com/us/app/mabells-zoo-interactive-counting/id712415904?ign-mpt=uo%3D4'}>
+								The App Store
+							</Button>
+							{/* <Button variant="primary" size="lg">
+								Google Play
+							</Button> */}
+						</div>
+						
+					</Container>
 				</Col>
 			</Row>
 			<Row>
